c7d2c31a7e782df62f093ebd371cfccbb2c4d60c
pnhung177 <pnhung177@acegik.net>
2019-04-20 11:10:22 +0700

    JSON response body comparison

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/opwire/opwire-testa
 go 1.12
 
 require (
-	github.com/google/go-cmp v0.2.0
-	github.com/gorilla/http v0.0.0-20180526232503-732371cf4733 // indirect
+	github.com/google/go-cmp v0.2.1-0.20190312032427-6f77996f0c42
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
 	github.com/urfave/cli v1.20.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2
--- lib/engine/SpecHandler.go
+++ lib/engine/SpecHandler.go
@@ -3,6 +3,8 @@ package engine
 import(
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"strings"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -67,25 +69,40 @@ func (e *SpecHandler) Examine(scenario *Scenario) (*ExaminationResult, error) {
 					errors["Body"] = fmt.Errorf("Response body is mismatched with expected content.\n    Received: %s\n    Expected: %s", _rb, *_eb.EqualTo)
 				}
 			}
-			if _eb.JSONEquals != nil {
+			if _eb.JSONEquals != nil || _eb.JSONCovers != nil {
 				var receivedJSON, expectedJSON interface{}
 				next := true
 				if (res.Body == nil) {
-					errors["Body/JSONEquals/empty"] = fmt.Errorf("Response body is empty (invalid JSON)")
+					errors["Body/receivedJSON"] = fmt.Errorf("Response body is empty (invalid JSON)")
+					next = false
+				} else if err := json.Unmarshal(res.Body, &receivedJSON); err != nil {
+					errors["Body/receivedJSON"] = fmt.Errorf("Invalid response content: %s", err)
 					next = false
 				}
-				if err := json.Unmarshal(res.Body, &receivedJSON); err != nil {
-					errors["Body/JSONEquals/received"] = fmt.Errorf("Invalid response content: %s", err)
-					next = false
+				if next && _eb.JSONEquals != nil {
+					if err := json.Unmarshal([]byte(*_eb.JSONEquals), &expectedJSON); err != nil {
+						errors["Body/expectedJSON"] = fmt.Errorf("Invalid expected content: %s", err)
+						next = false
+					}
+					if next {
+						if diff := cmp.Diff(expectedJSON, receivedJSON); diff != "" {
+							errors["Body/JSONEquals"] = fmt.Errorf("Body mismatch (-expected +received):\n%s", diff)
+						}
+					}
 				}
-				if err := json.Unmarshal([]byte(*_eb.JSONEquals), &expectedJSON); err != nil {
-					errors["Body/JSONEquals/expected"] = fmt.Errorf("Invalid expected content: %s", err)
-					next = false
+				if next && _eb.JSONCovers != nil {
+					if err := json.Unmarshal([]byte(*_eb.JSONCovers), &expectedJSON); err != nil {
+						errors["Body/expectedJSON"] = fmt.Errorf("Invalid expected content: %s", err)
+						next = false
+					}
+					if next {
+						var r DiffReporter
+						diff := cmp.Diff(expectedJSON, receivedJSON, cmp.Reporter(&r))
+						if r.HasDiffs() {
+							errors["Body/JSONCovers"] = fmt.Errorf("Body mismatch (-expected +received):\n%s", diff)
+						}
+					}
 				}
-				if diff := cmp.Diff(expectedJSON, receivedJSON); diff != "" {
-					errors["Body/JSONEquals/result"] = fmt.Errorf("Body mismatch (-expected +received):\n%s", diff)
-				}
-				_ = next
 			}
 		}
 	}
@@ -126,13 +143,51 @@ type MeasureBody struct {
 	HasFormat *string `yaml:"has-format"`
 	MatchWith *string `yaml:"match-with"`
 	EqualTo *string `yaml:"equal-to"`
-	JSONEquals *string `yaml:"equal-json"`
-	JSONContains *string `yaml:"json-contains"`
-	YAMLEquals *string `yaml:"equal-yaml"`
-	YAMLContains *string `yaml:"yaml-contains"`
+	JSONEquals *string `yaml:"json-equal"`
+	JSONCovers *string `yaml:"json-include"`
+	YAMLEquals *string `yaml:"yaml-equal"`
+	YAMLCovers *string `yaml:"yaml-include"`
 }
 
 type ExaminationResult struct {
 	Errors map[string]error
 	Response *HttpResponse
+}
+
+type DiffReporter struct {
+	path  cmp.Path
+	diffs []string
+}
+
+func (r *DiffReporter) PushStep(ps cmp.PathStep) {
+	r.path = append(r.path, ps)
+}
+
+func (r *DiffReporter) Report(rs cmp.Result) {
+	if !rs.Equal() {
+		vx, vy := r.path.Last().Values()
+		if !IsZero(vx) {
+			r.diffs = append(r.diffs, fmt.Sprintf("%#v:\n\t-: %+v\n\t+: %+v\n", r.path, vx, vy))
+		}
+	}
+}
+
+func (r *DiffReporter) PopStep() {
+	r.path = r.path[:len(r.path)-1]
+}
+
+func (r *DiffReporter) String() string {
+	return strings.Join(r.diffs, "\n")
+}
+
+func (r *DiffReporter) HasDiffs() bool {
+	return len(r.diffs) > 0
+}
+
+func (r *DiffReporter) GetDiffs() []string {
+	return r.diffs
+}
+
+func IsZero(v reflect.Value) bool {
+	return !v.IsValid() || reflect.DeepEqual(v.Interface(), reflect.Zero(v.Type()).Interface())
 }
