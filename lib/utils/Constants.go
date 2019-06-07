0e67681e74d7585c1c1c7233d32f0d013816677e
pnhung177 <pnhung177@acegik.net>
2019-06-07 11:50:12 +0700

    Accept non empty test case title

--- lib/utils/Constants.go
+++ lib/utils/Constants.go
@@ -19,5 +19,6 @@ const TAG_PATTERN string = `[a-zA-Z][a-zA-Z0-9]*([_-][a-zA-Z0-9]*)*`
 const TIME_RFC3339 string = `([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))`
 const TIMEOUT_PATTERN string = `([0-9]+h)?([0-9]+m)?([0-9]+s)?([0-9]+ms)?([0-9]+[uµ]s)?([0-9]+ns)?`
 
-const TEST_CASE_TITLE_PATTERN string = `[a-zA-Z][\\w\\-\\s.:;,]*`
+const TEST_CASE_TITLE_PATTERN string = `.+`
+const TEST_CASE_TITLE_PATTERN_STRICT string = `[a-zA-Z][\\w\\-\\s.:;,]*`
 var TEST_CASE_TITLE_REGEXP *regexp.Regexp = regexp.MustCompile(`^` + strings.ReplaceAll(TEST_CASE_TITLE_PATTERN, `\\`, `\`) + `$`)
